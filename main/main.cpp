7ea2c6ea361833293ac4b3832499161b54c04e16
Fernando-Goncalves-Campos <nandito3990@gmail.com>
2023-10-08 11:26:42 -0300

    Alguns testes + comentários no código

--- main/main.cpp
+++ main/main.cpp
@@ -8,6 +8,8 @@
 
 using namespace std;
 
+
+//Compares which path is the best for A* algorithm
 class bestChoiceComparator{
 public:
     bool operator()(const array<int, 4>& pointA, const array<int, 4>& pointB){
@@ -16,23 +18,30 @@ public:
 };
 
 vector<vector<int>> bfs(Maze& maze){
+    //Saves the all the possible next paths to take
     queue<array<int, 3>> pathsQueue;
     pathsQueue.push({maze.startY, maze.startX, 0});
 
+    //Saves the solution to the maze
     vector<vector<int>> solution(maze.paths.size(), vector<int>(maze.paths.size(), -1));
 
     while(!pathsQueue.empty()){
+        //Looks at the next position
         const array<int, 3> curPath = pathsQueue.front();
         pathsQueue.pop();
 
+        //Saves the distance to the current position ftom the start
         solution[curPath[0]][curPath[1]] = curPath[2];
 
+        //Marks current positions as being visited
         char& curPos = maze.paths[curPath[0]][curPath[1]] |= Maze::paint::VISITED;        
 
+        //Checks if found the goal
         if(curPos & Maze::paint::GOAL){
             return solution;
         }
 
+        //Try going to the adjacent spaces
         if((curPos & Maze::direction::UP) && (maze.paths[curPath[0] - 1][curPath[1]] & Maze::paint::VISITED) == 0){
             pathsQueue.push({curPath[0] - 1, curPath[1], curPath[2] + 1});
         }
@@ -51,23 +60,30 @@ vector<vector<int>> bfs(Maze& maze){
 }
 
 vector<vector<int>> aStar(Maze& maze){
+    //Saves the all the possible next paths to take
     priority_queue<array<int,4>, vector<array<int, 4>>, bestChoiceComparator> pathsQueue;
     pathsQueue.push({maze.startY, maze.startX, 0, abs(maze.endY - maze.startY) + abs(maze.endX - maze.startX)});
 
+    //Saves the solution to the maze
     vector<vector<int>> solution(maze.paths.size(), vector<int>(maze.paths.size(), -1));
 
     while(!pathsQueue.empty()){
+        //Looks at the next position
         const array<int, 4> curPath = pathsQueue.top();
         pathsQueue.pop();
 
+        //Saves the distance to the current position ftom the start
         solution[curPath[0]][curPath[1]] = curPath[2];
 
+        //Marks current positions as being visited
         char& curPos = maze.paths[curPath[0]][curPath[1]] |= Maze::paint::VISITED;
 
+        //Checks if found the goal
         if(curPos & Maze::paint::GOAL){
             return solution;
         }
 
+        //Try going to the adjacent spaces
         if((curPos & Maze::direction::UP) && (maze.paths[curPath[0] - 1][curPath[1]] & Maze::paint::VISITED) == 0){
             pathsQueue.push({curPath[0] - 1, curPath[1], curPath[2] + 1,
                              curPath[2] + 1 + abs(maze.endY - (curPath[0] - 1)) + abs(maze.endX - curPath[1])});
@@ -90,25 +106,54 @@ vector<vector<int>> aStar(Maze& maze){
 }
 
 int main(){
-    Maze testMaze(20, 3, 3, 18, 18, 7, 7);
+    //=======================Image testing=======================
+    /*Maze testMaze(20, 3, 3, 18, 18, 7, 7);
     Maze bfsMaze = testMaze;
     Maze aStarMaze = testMaze;
 
     vector<vector<int>> solutionAStar = aStar(aStarMaze);
     vector<vector<int>> solutionBFS = bfs(bfsMaze);
-    //cout << "A*: " << perftest::benchmark(aStar, aStarMaze) << "ms" << endl;
-    //cout << "BFS: " << perftest::benchmark(bfs, bfsMaze) << "ms" << endl;
 
-    //Maze::mazeToImage("unsolvedMaze", testMaze);
-    //Maze::mazeToImage("bfsMaze", bfsMaze);
-    //Maze::mazeToImage("aStarMaze", aStarMaze);
-    //Maze::colorSolution(solutionBFS, bfsMaze);
-    //Maze::colorSolution(solutionAStar, aStarMaze);
-    //Maze::mazeToImage("bfsMazeSolution", bfsMaze);
-    //Maze::mazeToImage("aStarMazeSolution", aStarMaze);
+    Maze::mazeToImage("unsolvedMaze", testMaze);
+    Maze::mazeToImage("bfsMaze", bfsMaze);
+    Maze::mazeToImage("aStarMaze", aStarMaze);
+    Maze::colorSolution(solutionBFS, bfsMaze);
+    Maze::colorSolution(solutionAStar, aStarMaze);
+    Maze::mazeToImage("bfsMazeSolution", bfsMaze);
+    Maze::mazeToImage("aStarMazeSolution", aStarMaze);*/
 
-    //cout << "A* to image duration: " << perftest::benchmark(Maze::mazeToImage, "aStarMaze.ppm", aStarMaze) << "ms\n";
-    //cout << "BFS to image duration: " << perftest::benchmark(Maze::mazeToImage, "bfsMaze.ppm", bfsMaze) << "ms\n";
+
+    //=======================Performance testing=======================
+    /*vector<int> sizes{10, 20, 30, 40, 50, 60, 70};
+    const int nRepeats = 10;
+
+    /*vector<vector<double>> bfsTimes(sizes.size(), vector<double>(2, 0));
+    for(int i = 0; i < sizes.size(); i++){
+        const int curSize = sizes[i];
+        double totalDuration = 0;
+        for(int j = nRepeats; j >= 0; j--){
+            clog << "\rMaze size: " << curSize << " Remaining tests: " << j << flush;
+            Maze testMaze(curSize, curSize * 0.15, curSize * 0.15, curSize * 0.9, curSize * 0.9, curSize * 0.5, curSize * 0.5);
+            totalDuration += perftest::benchmark(bfs, testMaze);
+        }
+        bfsTimes[i][0] = curSize;
+        bfsTimes[i][1] = totalDuration / nRepeats;
+    }
+    cout << "Bfs results (ms):\n" << bfsTimes << "\n";*/
+
+    /*vector<vector<double>> aStarTimes(sizes.size(), vector<double>(2, 0));
+    for(int i = 0; i < sizes.size(); i++){
+        const int curSize = sizes[i];
+        double totalDuration = 0;
+        for(int j = nRepeats; j >= 0; j--){
+            clog << "\rMaze size: " << curSize << " Remaining tests: " << j << flush;
+            Maze testMaze(curSize, curSize * 0.15, curSize * 0.15, curSize * 0.9, curSize * 0.9, curSize * 0.5, curSize * 0.5);
+            totalDuration += perftest::benchmark(aStar, testMaze);
+        }
+        aStarTimes[i][0] = curSize;
+        aStarTimes[i][1] = totalDuration / nRepeats;
+    }
+    cout << "Bfs results (ms):\n" << aStarTimes << "\n";*/
 
     return 0;
 }
--- src/maze/maze.hpp
+++ src/maze/maze.hpp
@@ -19,26 +19,31 @@ std::vector<std::vector<char>> paths;
 Maze(const int size, const int startY_, const int startX_,
      const int endY_, const int endX_,
      const int originY = 0, const int originX = 0) : paths(size, std::vector<char>(size, 0)){
-    paths[originY][originX] |= paint::PATH; 
+    //Mark the start and goal of the maze
     paths[startY_][startX_] |= paint::START;
     paths[endY_][endX_] |= paint::GOAL;
-    
+
+    //Save the start and goal of the maze
     startY = startY_;
     startX = startX_;
     endY = endY_;
     endX = endX_;
 
+    //Creates a maze the that uses sizeXsize spaces
     if(size > 1){
         expandMaze(originY, originX);
     }
 }
 
 static void mazeToImage(const std::string&& filename, const Maze& mazeObj){
+    //Add walls to the maze to make easier to visualize
     const std::vector<std::vector<char>> newMaze = mazeObj.placeWalls();
     
+    //Creates a .ppm image file in the images folder of the projext
     std::fstream fout(std::string("../images/ppmFiles/") + filename + ".ppm", std::ofstream::out | std::ofstream::trunc);
     fout << "P3\n" << newMaze.size() << ' ' << newMaze.size() << "\n255\n";
 
+    //Add all the pixels of the image
     for (int y = 0; y < newMaze.size(); ++y) {
         for (int x = 0; x < newMaze.size(); ++x) {
             double r = 0;
@@ -82,6 +87,7 @@ static void colorSolution(const std::vector<std::vector<int>>& solution, Maze& m
         return;
     }
 
+    //Finds the minimun distance path from the start to the goal, starting from the goal
     while(true){
         if((maze.paths[curY][curX] & direction::UP) && (solution[curY-1][curX] == curDist-1)){
             if(--curDist == 0){
@@ -112,14 +118,17 @@ static void colorSolution(const std::vector<std::vector<int>>& solution, Maze& m
 
 private:
 std::vector<std::vector<char>> placeWalls() const {
+    //Creates a new grid that can hold the walls
     int size = paths.size();
     const int newSize = (size<<1)+1;
     std::vector<std::vector<char>> newMaze(newSize, std::vector<char>(newSize, 0));
 
     for(int y = 0; y < size; y++){
         for(int x = 0; x < size; x++){
+            //Copies the original spaces from the maze
             newMaze[(y << 1) + 1][(x << 1) + 1] = paths[y][x];
 
+            //Adds the colors to horizontal passages in walls
             if(x && (paths[y][x] & direction::LEFT)){
                 newMaze[(y << 1) + 1][x << 1] |= (direction::LEFT | direction::RIGHT | paint::PATH);
 
@@ -131,6 +140,7 @@ std::vector<std::vector<char>> placeWalls() const {
                 }
             }
 
+            //Adds the colors to vertical passages in walls
             if(y && (paths[y][x] & direction::UP)){
                 newMaze[y << 1][(x << 1) + 1] |= (direction::UP | direction::DOWN | paint::PATH);
 
@@ -148,8 +158,10 @@ std::vector<std::vector<char>> placeWalls() const {
 }
 
 void expandMaze(const int curY, const int curX){
+    //Marks the current position as being part of the maze
     paths[curY][curX] |= paint::PATH;
     
+    //Adds the adjacent walls to a list
     std::list<std::array<int, 3>> walls;
     if(curY != 0) 
         walls.push_back({curY - 1, curX, direction::DOWN});
@@ -160,15 +172,19 @@ void expandMaze(const int curY, const int curX){
     if(curX != 0) 
         walls.push_back({curY, curX - 1, direction::RIGHT});
 
+    //Saves wall that stayed in the maze
     std::list<std::array<int, 3>> leftOverWalls;
 
     int nSelected = 0;
     std::random_device rd;
     while(!walls.empty()){
+        //Selects a random wall from the list
         const auto curWallIt = std::next(walls.begin(), rd() % walls.size());
         const std::array<int, 3> curWall = *curWallIt;
         walls.erase(curWallIt);
 
+        //Tests if "opening" the wall will add a new position to the maze
+        //In case the next position already is part of the maze, the wall is not "opened"
         if(paths[curWall[0]][curWall[1]] & paint::PATH){
             if(paths[curWall[0]][curWall[1]] & curWall[2]){
                 nSelected++;
@@ -179,6 +195,7 @@ void expandMaze(const int curY, const int curX){
             continue;
         }
 
+        //Add the directions to both positions tha can use the opened path
         nSelected++;
         paths[curWall[0]][curWall[1]] |= curWall[2];
         switch(curWall[2]){
@@ -194,6 +211,7 @@ void expandMaze(const int curY, const int curX){
         expandMaze(curWall[0], curWall[1]);
     }
 
+    //Makes sure that the position isn't a dead end, unless is in the edges of the maze
     if(nSelected == 1 && curX != 0 && curX != paths.size()-1 && curY != 0 && curY != paths.size()-1){
         const std::array<int, 3> curWall = *std::next(leftOverWalls.begin(), rd() % leftOverWalls.size());
 
