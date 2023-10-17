67681f86473eaa5310484e894a260875f8f2450a
Fernando-Goncalves-Campos <nandito3990@gmail.com>
2023-10-17 13:04:04 -0300

    DFS e Best-First

--- main/main.cpp
+++ main/main.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include <queue>
+#include <stack>
 #include <array>
+#include <algorithm>
 
-#include "../src/maze/maze.hpp"
+#include "../src/paths/paths.hpp"
+#include "../src/paths/maze.hpp"
+
 #include "../src/utils/perftest.hpp"
 #include "../src/utils/printtemplates.hpp"
 #include "../src/utils/createCSV.hpp"
 
 using namespace std;
-
 
 //Compares which path is the best for A* algorithm
 class bestChoiceComparator{
@@ -41,7 +44,7 @@ vector<vector<int>> bfs(Maze& maze){
         char& curPos = maze.paths[curPath[0]][curPath[1]] |= Maze::paint::VISITED;        
 
         //Creates a frame that will be added to a GIF
-        //Maze::mazeToImage(string("../GIFs/bfsFrames/frame_") + to_string(frameCount++), maze);
+        //maze.mazeToImage(string("../GIFs/bfsFrames/frame_") + to_string(frameCount++));
 
         //Checks if found the goal
         if(curPos & Maze::paint::GOAL){
@@ -89,7 +92,7 @@ vector<vector<int>> aStar(Maze& maze){
         char& curPos = maze.paths[curPath[0]][curPath[1]] |= Maze::paint::VISITED;
 
         //Creates a frame that will be added to a GIF
-        //Maze::mazeToImage(string("../GIFs/aStarFrames/frame_") + to_string(frameCount++), maze);
+        //maze.mazeToImage(string("../GIFs/aStarFrames/frame_") + to_string(frameCount++));
 
         //Checks if found the goal
         if(curPos & Maze::paint::GOAL){
@@ -118,29 +121,146 @@ vector<vector<int>> aStar(Maze& maze){
     return solution;
 }
 
+vector<vector<int>> dfs(Maze& maze){
+    //Saves the all the possible next paths to take
+    stack<array<int, 3>> pathsStack;
+    pathsStack.push({maze.startY, maze.startX, 0});
+
+    //Saves the solution to the maze
+    vector<vector<int>> solution(maze.paths.size(), vector<int>(maze.paths.size(), -1));
+
+    //Used for creating gifs
+    //int frameCount = 0;
+
+    while(!pathsStack.empty()){
+        //Looks at the next position
+        const array<int, 3> curPath = pathsStack.top();
+        pathsStack.pop();
+
+        //Saves the distance to the current position ftom the start
+        solution[curPath[0]][curPath[1]] = curPath[2];
+
+        //Marks current positions as being visited
+        char& curPos = maze.paths[curPath[0]][curPath[1]] |= Maze::paint::VISITED;        
+
+        //Creates a frame that will be added to a GIF
+        //maze.mazeToImage(string("../GIFs/dfsFrames/frame_") + to_string(frameCount++));
+
+        //Checks if found the goal
+        if(curPos & Maze::paint::GOAL){
+            return solution;
+        }
+
+        //Try going to the adjacent spaces
+        if((curPos & Maze::direction::UP) && (maze.paths[curPath[0] - 1][curPath[1]] & Maze::paint::VISITED) == 0){
+            pathsStack.push({curPath[0] - 1, curPath[1], curPath[2] + 1});
+        }
+        if((curPos & Maze::direction::RIGHT) && (maze.paths[curPath[0]][curPath[1] + 1] & Maze::paint::VISITED) == 0){
+            pathsStack.push({curPath[0], curPath[1] + 1, curPath[2] + 1});
+        }
+        if((curPos & Maze::direction::DOWN) && (maze.paths[curPath[0] + 1][curPath[1]] & Maze::paint::VISITED) == 0){
+            pathsStack.push({curPath[0] + 1, curPath[1], curPath[2] + 1});
+        }
+        if((curPos & Maze::direction::LEFT) && (maze.paths[curPath[0]][curPath[1] - 1] & Maze::paint::VISITED) == 0){
+            pathsStack.push({curPath[0], curPath[1] - 1, curPath[2] + 1});
+        }
+    }
+
+    return solution;
+}
+
+vector<vector<int>> bestFirst(Maze& maze){
+    //Saves the all the possible next paths to take
+    stack<array<int, 4>> pathsStack;
+    pathsStack.push({maze.startY, maze.startX, 0, abs(maze.endY - maze.startY) + abs(maze.endX - maze.startX)});
+
+    //Saves the solution to the maze
+    vector<vector<int>> solution(maze.paths.size(), vector<int>(maze.paths.size(), -1));
+
+    //Used for creating gifs
+    //int frameCount = 0;
+
+    while(!pathsStack.empty()){
+        //Looks at the next position
+        const array<int, 4> curPath = pathsStack.top();
+        pathsStack.pop();
+
+        //Saves the distance to the current position ftom the start
+        solution[curPath[0]][curPath[1]] = curPath[2];
+
+        //Marks current positions as being visited
+        char& curPos = maze.paths[curPath[0]][curPath[1]] |= Maze::paint::VISITED;        
+
+        //Creates a frame that will be added to a GIF
+        //maze.mazeToImage(string("../GIFs/bestFirstFrames/frame_") + to_string(frameCount++));
+
+        //Checks if found the goal
+        if(curPos & Maze::paint::GOAL){
+            return solution;
+        }
+
+        //Try going to the adjacent spaces
+        vector<array<int, 4>> adjSpaces;
+        if((curPos & Maze::direction::UP) && (maze.paths[curPath[0] - 1][curPath[1]] & Maze::paint::VISITED) == 0){
+            adjSpaces.push_back({curPath[0] - 1, curPath[1], curPath[2] + 1,
+            abs(maze.endY - (curPath[0] - 1)) + abs(maze.endX - curPath[1])});
+        }
+        if((curPos & Maze::direction::RIGHT) && (maze.paths[curPath[0]][curPath[1] + 1] & Maze::paint::VISITED) == 0){
+            adjSpaces.push_back({curPath[0], curPath[1] + 1, curPath[2] + 1,
+            abs(maze.endY - curPath[0]) + abs(maze.endX - (curPath[1] + 1))});
+        }
+        if((curPos & Maze::direction::DOWN) && (maze.paths[curPath[0] + 1][curPath[1]] & Maze::paint::VISITED) == 0){
+            adjSpaces.push_back({curPath[0] + 1, curPath[1], curPath[2] + 1,
+            abs(maze.endY - (curPath[0] + 1)) + abs(maze.endX - curPath[1])});
+        }
+        if((curPos & Maze::direction::LEFT) && (maze.paths[curPath[0]][curPath[1] - 1] & Maze::paint::VISITED) == 0){
+            adjSpaces.push_back({curPath[0], curPath[1] - 1, curPath[2] + 1,
+            abs(maze.endY - curPath[0]) + abs(maze.endX - (curPath[1] - 1))});
+        }
+
+        //Sorts the adjacent spaces considering their distance to the end
+        sort(adjSpaces.begin(), adjSpaces.end(), [](const array<int, 4>& pointA, const array<int, 4>& pointB) -> bool {
+            return pointA[3] > pointB[3];
+        });
+
+        for(int i = 0; i < adjSpaces.size(); i++){
+            pathsStack.push(adjSpaces[i]);
+        }
+    }
+
+    return solution;
+}
+
 void createExamples(const int size, const int startY_, const int startX_,
                     const int endY_, const int endX_,
                     const int originY = 0, const int originX = 0, const bool allowDeadEnds = false){
     Maze testMaze(size, startY_, startX_, endY_, endX_, originY, originX, allowDeadEnds);
     Maze bfsMaze = testMaze;
     Maze aStarMaze = testMaze;
+    Maze dfsMaze = testMaze;
+    Maze bestFirstMaze = testMaze;
 
     //It's possible to create frames to be converted to a gif (this might increase substantially the execution time)
-    //Make sure that the lines: 30, 44, 78 and 92 are not commented
+    //Make sure that the lines for creating frames are not commented in the function
+    vector<vector<int>> solutionBfs = bfs(bfsMaze);
     vector<vector<int>> solutionAStar = aStar(aStarMaze);
-    vector<vector<int>> solutionBFS = bfs(bfsMaze);
-
+    vector<vector<int>> solutionDfs = dfs(dfsMaze);
+    vector<vector<int>> solutionBestFirst = bestFirst(bestFirstMaze);
 
     //Create ppm images to be converted to png files
-    /*Maze::mazeToImage("../images/ppmFiles/unsolvedMaze", testMaze);
-    Maze::mazeToImage("../images/ppmFiles/bfsMaze", bfsMaze);
-    Maze::mazeToImage("../images/ppmFiles/aStarMaze", aStarMaze);
-    Maze::colorSolution(solutionBFS, bfsMaze);
-    Maze::colorSolution(solutionAStar, aStarMaze);
-    Maze::mazeToImage("../images/ppmFiles/bfsMazeSolution", bfsMaze);
-    Maze::mazeToImage("../images/ppmFiles/aStarMazeSolution", aStarMaze);*/
-
-    
+    testMaze.mazeToImage("../images/ppmFiles/unsolvedMaze");
+    bfsMaze.mazeToImage("../images/ppmFiles/bfsMaze");
+    aStarMaze.mazeToImage("../images/ppmFiles/aStarMaze");
+    dfsMaze.mazeToImage("../images/ppmFiles/dfsMaze");
+    bestFirstMaze.mazeToImage("../images/ppmFiles/bestFirstMaze");
+    bfsMaze.colorSolution(solutionBfs);
+    aStarMaze.colorSolution(solutionAStar);
+    dfsMaze.colorSolution(solutionDfs);
+    bestFirstMaze.colorSolution(solutionBestFirst);
+    bfsMaze.mazeToImage("../images/ppmFiles/bfsMazeSolution");
+    aStarMaze.mazeToImage("../images/ppmFiles/aStarMazeSolution");
+    dfsMaze.mazeToImage("../images/ppmFiles/dfsMazeSolution", solutionDfs);
+    bestFirstMaze.mazeToImage("../images/ppmFiles/bestFirstMazeSolution", solutionBestFirst);
 }
 
 vector<vector<double>> timeMultipleSolves(auto func, const int nRepeats, const vector<int>& sizes){
@@ -161,11 +281,9 @@ vector<vector<double>> timeMultipleSolves(auto func, const int nRepeats, const v
     return times;
 }
 
-int main(){
+void main_(){
     //=======================Image testing=============================
-    double totalTime = perftest::benchmark(createExamples, 30, 5, 5, 25, 25, 10, 10, false);
-    cout << "Total duration for creation of examples: " << totalTime << "ms\n";
-
+    createExamples(30, 5, 5, 25, 25, 10, 10, false);
 
     //=======================Performance testing=======================
     //vector<int> sizes{10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
@@ -179,5 +297,16 @@ int main(){
     cout << "A* results (ms):\n" << aStarTimes << "\n\n";
     createCSV(string("../CSVs/aStarResults"), vector<string>{"Size", "Duration"}, aStarTimes);*/
 
-    return 0;
+    /*vector<vector<double>> dfsTimes = timeMultipleSolves(dfs, nRepeats, sizes);
+    cout << "Dfs results (ms):\n" << dfsTimes << "\n\n";
+    createCSV(string("../CSVs/dfsResults"), vector<string>{"Size", "Duration"}, dfsTimes);*/
+
+    /*vector<vector<double>> bestFirstTimes = timeMultipleSolves(bestFirst, nRepeats, sizes);
+    cout << "Best First results (ms):\n" << bestFirstTimes << "\n\n";
+    createCSV(string("../CSVs/bestFirstResults"), vector<string>{"Size", "Duration"}, bestFirstTimes);*/
+}
+
+int main(){
+    double execTime = perftest::benchmark(main_);
+    cout << "Execution time: " << execTime << "ms\n";
 }
---
+++ src/paths/paths.hpp
@@ -0,0 +1,97 @@
+#pragma once
+
+#include <vector>
+#include <fstream>
+
+class Paths{
+public:
+    int startY, startX, endY, endX;
+
+    enum direction{UP = 1, RIGHT = 2, DOWN = 4, LEFT = 8};
+    enum paint{PATH = 16, VISITED = 32, START = 64, GOAL = 128, SOLUTION = 192};
+
+    std::vector<std::vector<char>> paths;
+
+    Paths(const size_t size, const int startY_, const int startX_,
+          const int endY_, const int endX_) 
+          : paths(size, std::vector<char>(size, 0)){
+        startY = startY_;
+        startX = startX_;
+        endY = endY_;
+        endX = endX_;
+    };
+
+    static void createImage(const std::string& filename, const std::vector<std::vector<char>>& paths){
+        //Creates a .ppm image file in the images folder of the project
+        std::fstream fout(filename + ".ppm", std::ofstream::out | std::ofstream::trunc);
+        fout << "P3\n" << paths.size() << ' ' << paths[0].size() << "\n255\n";
+
+        //Add all the pixels of the image
+        for (int y = 0; y < paths.size(); ++y) {
+            for (int x = 0; x < paths[0].size(); ++x) {
+                double r = 0;
+                double g = 0;
+                double b = 0;
+
+                if((paths[y][x] & paint::SOLUTION) == paint::SOLUTION){
+                    g = b = 1;
+                }
+                else if(paths[y][x] & paint::START){
+                    g = 1;
+                }
+                else if(paths[y][x] & paint::GOAL){
+                    b = 1;
+                }
+                else if(paths[y][x] & paint::VISITED){
+                    r = 1;
+                }
+                else if(paths[y][x] & paint::PATH){
+                    r = 1;
+                    g = 1;
+                    b = 1;
+                }
+
+                int ir = static_cast<int>(255.999 * r);
+                int ig = static_cast<int>(255.999 * g);
+                int ib = static_cast<int>(255.999 * b);
+
+                fout << ir << ' ' << ig << ' ' << ib << '\n';
+            }
+        }
+        fout.close();
+    }
+
+    void colorSolution(const std::vector<std::vector<int>>& solution){
+        int curY = endY;
+        int curX = endX;
+        int curDist = solution[curY][curX];
+        
+        if(curDist == -1){
+            return;
+        }
+
+        //Finds the minimun distance path from the start to the goal, starting from the goal
+        while(true){
+            if((paths[curY][curX] & direction::UP) && (solution[curY-1][curX] == curDist-1)){
+                if(--curDist == 0)
+                    return;
+                paths[--curY][curX] |= paint::SOLUTION;
+            }
+            else if((paths[curY][curX] & direction::RIGHT) && (solution[curY][curX+1] == curDist-1)){
+                if(--curDist == 0)
+                    return;
+                paths[curY][++curX] |= paint::SOLUTION;
+            }
+            else if((paths[curY][curX] & direction::DOWN) && (solution[curY+1][curX] == curDist-1)){
+                if(--curDist == 0)
+                    return;
+                paths[++curY][curX] |= paint::SOLUTION;
+            }
+            else if((paths[curY][curX] & direction::LEFT) && (solution[curY][curX-1] == curDist-1)){
+                if(--curDist == 0)
+                    return;
+                paths[curY][--curX] |= paint::SOLUTION;
+            }
+        }
+    }
+};
